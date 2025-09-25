60debc59df8fc32acc2573b63db89e1fa14bf7f3
bibekparajuli07 <bibekparajuli29#gmail.com>
2025-09-25 23:02:45 +0545

    first commit

---
+++ lib/curr_converter.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CurrConverter extends StatelessWidget {
+  const CurrConverter({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("hello world!!")));
+  }
+}
