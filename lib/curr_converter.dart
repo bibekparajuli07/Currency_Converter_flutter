0b57cda756da522bb0d812c421cad1a9d8debdfa
bibekparajuli07 <bibekparajuli29#gmail.com>
2025-09-27 14:53:00 +0545

    2nd commit

--- lib/curr_converter.dart
+++ lib/curr_converter.dart
@@ -4,6 +4,25 @@ class CurrConverter extends StatelessWidget {
   const CurrConverter({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("hello world!!")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: "Enter Amount",
+                suffixIcon: Icon(Icons.monetization_on),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
